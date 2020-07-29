c9dd5b783cc8e50d4598c618d5d8403bcee63d05
xSakix <seckarovci@gmail.com>
2020-07-29 23:05:41 +0200

    Checking whether the game is finished is implemented [in an ugly way], but I got and error in my main test scenario. X shapes don't create a line so let's fix that...

--- src/main/src/tic/tac/toe/TicTacToeGame.java
+++ src/main/src/tic/tac/toe/TicTacToeGame.java
@@ -11,13 +11,96 @@ public class TicTacToeGame  {
 
             @Override
             public boolean isFinished() {
+
+                for(int row = 0; row < MAX_ROW; row++){
+                    int count_o = 0;
+                    int count_x = 0;
+                    for(int col = 0; col < MAX_COL;col++){
+                        if(isEmptySlot(grid[row][col])){
+                           break;
+                        }
+
+                        if(grid[row][col] instanceof OShape){
+                            count_o++;
+                        }else if(grid[row][col] instanceof XShape){
+                            count_x++;
+                        }
+                    }
+
+                    if(count_o == 3 || count_x == 3){
+                        return true;
+                    }
+                }
+
+                for(int col = 0; col < MAX_COL;col++){
+                    int count_o = 0;
+                    int count_x = 0;
+                    for(int row = 0; row < MAX_ROW; row++){
+                        if(isEmptySlot(grid[row][col])){
+                            break;
+                        }
+
+                        if(grid[row][col] instanceof OShape){
+                            count_o++;
+                        }else if(grid[row][col] instanceof XShape){
+                            count_x++;
+                        }
+                    }
+
+                    if(count_o == 3 || count_x == 3){
+                        return true;
+                    }
+                }
+
+                int[][] diag  = new int[][]{{0,0},{1,1},{2,2}};
+                int count_o = 0;
+                int count_x = 0;
+                for(int[] coord : diag){
+                    if(isEmptySlot(grid[coord[0]][coord[1]])){
+                        break;
+                    }
+
+                    if(grid[coord[0]][coord[1]] instanceof OShape){
+                        count_o++;
+                    }else if(grid[coord[0]][coord[1]] instanceof XShape){
+                        count_x++;
+                    }
+                    if(count_o == 3 || count_x == 3){
+                        return true;
+                    }
+
+                }
+
+                diag  = new int[][]{{2,0},{1,1},{0,2}};
+                count_o = 0;
+                count_x = 0;
+                for(int[] coord : diag){
+                    if(isEmptySlot(grid[coord[0]][coord[1]])){
+                        break;
+                    }
+
+                    if(grid[coord[0]][coord[1]] instanceof OShape){
+                        count_o++;
+                    }else if(grid[coord[0]][coord[1]] instanceof XShape){
+                        count_x++;
+                    }
+                    if(count_o == 3 || count_x == 3){
+                        return true;
+                    }
+
+                }
+
                 return false;
+            }
+
+            private boolean isEmptySlot(GameShape gameShape) {
+                return gameShape == null;
             }
 
             @Override
             public boolean canAdd(GameShape shape) {
 
-                return grid[shape.row()][shape.column()] == null;
+                return isEmptySlot(grid[shape.row()][shape.column()]);
             }
 
             @Override
