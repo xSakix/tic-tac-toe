895a9a2fa4dd737b1ae8cd904fdb061c49782164
xSakix <seckarovci@gmail.com>
2020-07-29 20:52:03 +0200

    base project structure with maven and junit4 tests

---
+++ src/test/tic/tac/toe/GameTest.java
@@ -0,0 +1,13 @@
+package tic.tac.toe;
+
+import org.junit.Test;
+import static org.junit.Assert.fail;
+
+public class GameTest{
+
+    @Test
+    public void start(){
+        fail("Not implemented");
+    }
+
+}
