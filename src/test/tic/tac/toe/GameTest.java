cc048aed562fe20e53b920a670dfd0f0b18c82ec
xSakix <seckarovci@gmail.com>
2020-07-29 21:24:48 +0200

    First scenario test which fails on not implemented classes/types/etc

--- src/test/tic/tac/toe/GameTest.java
+++ src/test/tic/tac/toe/GameTest.java
@@ -1,13 +1,48 @@
 package tic.tac.toe;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.fail;
 
 public class GameTest{
 
     @Test
-    public void start(){
-        fail("Not implemented");
+    public void playGameScenario(){
+        Game game = TicTacToeGame.start();
+        Assert.assertNotNull(game);
+        Assert.assertTrue(game.isStarted());
+        Assert.assertFalse(game.isFinished());
+
+        GameShape oShape = ShapeFactory.newOShape(GridPosition.ONE);
+        Assert.assertNotNull(oShape);
+        Assert.assertTrue(game.canAdd(oShape));
+        game.add(oShape);
+        Assert.assertFalse(game.isFinished());
+
+        GameShape xShape = ShapeFactory.newXShape(GridPosition.ONE);
+        Assert.assertNotNull(xShape);
+        Assert.assertFalse(game.canAdd(xShape));
+
+        xShape = ShapeFactory.newXShape(GridPosition.TWO);
+        Assert.assertNotNull(xShape);
+        Assert.assertTrue(game.canAdd(xShape));
+        game.add(xShape);
+        Assert.assertFalse(game.isFinished());
+
+        List<GameShape> shapesToAdd = new ArrayList<>();
+        shapesToAdd.add(ShapeFactory.newXShape(GridPosition.FOUR));
+        shapesToAdd.add(ShapeFactory.newXShape(GridPosition.SEVEN));
+        for(GameShape shape: shapesToAdd){
+            Assert.assertNotNull(shape);
+            Assert.assertTrue(game.canAdd(shape));
+        }
+
+        Assert.assertTrue(game.isFinished());
     }
+
+
 
 }
